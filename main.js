162f937460c49f00ad8ad9cf7209b45e19ad0472
whenipush <sunsetbb@yahoo.com>
2023-07-20 14:18:49 +0300

    add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+item_1 = 5;
+console.log(item_1);
+item_2 = 3 ;
+console.log(item_2);
+item_3 = item_1+item_2; // 8
+console.log(item_3);
+item_4 = 'Yolochka';
+console.log(item_4);
+console.log(item_3+item_4);
+console.log(item_3*item_4);
+item_5 = item_3;
+item_6 = 15;
+item_6_type = typeof item_6;
+console.log('item_6 == '+ item_6, 'item_6_type == '+item_6_type );
+item_7 = String(item_6);
+item_7_type = item_7
